39fbfe21a086b00e739dff64da3bd42d4a92f002
Eric Martel <shrodi@gmail.com>
2023-09-30 11:02:49 -0400

    Test

--- DemarrerNotes.cmd
+++ DemarrerNotes.cmd
@@ -9,6 +9,7 @@ if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && 
 
 :: Met à jour le dépôt Git (nécessite que «Git for Windows» soit installé: https://gitforwindows.org/)
 git checkout .
+git pull
 
 :: Démarre les notes de cours avec le fureteur par défaut
 for %%I in (*.html) do start %%~nxI
