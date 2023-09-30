485aec3b5a493b39e6c072ecadf944188ca61748
Eric Martel <shrodi@gmail.com>
2023-09-30 10:58:40 -0400

    Change script démarrage

--- DemarrerNotes.cmd
+++ DemarrerNotes.cmd
@@ -4,15 +4,13 @@
 :: Pour que ce script fonctionne, il doit être situé dans le dossier
 :: des notes de cours, et Git pour Windows doit être installé.
 
-
 :: Redémarre minimisé (donc on ne devrait voir que très brièvement la fenêtre MS-DOS)
 if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
 
 :: Met à jour le dépôt Git (nécessite que «Git for Windows» soit installé: https://gitforwindows.org/)
-git pull
+git checkout .
 
-:: Démarre les notes de cours (ayant même nom que dossier courant) avec le fureteur par défaut
-::for %%I in (.) do start %%~nxI.html
+:: Démarre les notes de cours avec le fureteur par défaut
 for %%I in (*.html) do start %%~nxI
 
 exit
